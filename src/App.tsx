be861edf9caccfdee739cd25934ad56601969c65
zhengx55 <williamzhxin@gmail.com>
2023-10-28 12:57:53 -0400

    csss ui lib setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
-import "./global.css";
+import { Route, Routes } from "react-router-dom";
+import "./globals.css";
+import Home from "./_root/pages/Home";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,8 @@
+
+type Props = {};
+
+const AuthLayout = (props: Props) => {
+  return <div>AuthLayout</div>;
+};
+
+export default AuthLayout;
