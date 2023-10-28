350be0d968844ae419cbce4d715f975b2ea44450
zhengx55 <williamzhxin@gmail.com>
2023-10-28 15:10:48 -0400

    signupform

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,21 @@
+import { Navigate, Outlet } from "react-router-dom";
 
-type Props = {};
-
-const AuthLayout = (props: Props) => {
-  return <div>AuthLayout</div>;
+const AuthLayout = () => {
+  const isAuth = false;
+  return (
+    <>
+      {isAuth ? (
+        <Navigate to={"/"} />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-co py-10">
+            <Outlet />
+          </section>
+          <img src="/assets/images/side-img.svg" alt="logo" className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat" />
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: { "@": path.resolve(__dirname, "/.src") },
+    alias: { "@": path.resolve(__dirname, "./src") },
   },
 });
