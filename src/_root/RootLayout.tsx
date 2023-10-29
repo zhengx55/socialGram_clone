2a0e4cb58b4e39e95e21cb0f952b57d7d0818430
zhengx55 <williamzhxin@gmail.com>
2023-10-28 23:15:22 -0400

    mobile sidebar and topbar

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,5 +1,20 @@
+import { Outlet } from "react-router-dom";
+
+import Topbar from "@/components/Topbar";
+import Bottombar from "@/components/Bottombar";
+import LeftSidebar from "@/components/LeftSidebar";
+
 const RootLayout = () => {
-  return <div>RootLayout</div>;
+  return (
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+      <Bottombar />
+    </div>
+  );
 };
 
 export default RootLayout;
